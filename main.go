b51cc5508686c3f30166f8abbd8625ab92bbb92d
Christina Beckford <cbeckford70@gmail.com>
2020-11-27 21:48:57 -0500

    Add files via upload
    Lastest changes that have the client and pool

--- main.go
+++ main.go
@@ -5,60 +5,31 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-//define a reader which will listen for
-//new messages being sent to our WebSocket
-//endpoint
-func reader(conn *websocket.Conn) {
-	for {
-		//read in a message
-		messageType, p, err := conn.ReadMessage()
-
-		if err != nil {
-			log.Println(err)
-
-			return
-
-		}
-
-		//print out that message for clarity
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-
-	}
-}
 
 //define our WebSocket endpoint
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
+	fmt.Println("WebSocket Endpoint Hit") 
 
-	//upgrade this connection to a WebSocket
-	//connection
-	ws, err := upgrader.Upgrade(w, r, nil)
-
+	conn, err := websocket.Upgrade(w,r) 
 	if err != nil {
-		log.Println(err)
-	}
 
-	//listen indefinitely for new messages coming
-	//through on our WebSocket connection
+		fmt.Fprintf(w, "%+v\n", err)
+	} 
 
-	reader(ws)
+	client := &websocket.Client{
+		 
+		Conn: conn, 
+		Pool: pool,
+	} 
 
+	pool.Register <- client 
+	client.Read()
 }
+
+
 
 func setupRoutes() {
 
@@ -71,7 +42,7 @@ func setupRoutes() {
 }
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	fmt.Println(" Distributed Chat App v0.01")
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 
