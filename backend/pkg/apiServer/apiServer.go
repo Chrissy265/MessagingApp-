3335c01997f2034fe0c1d4a8f221325accb2f4c0
rsantangelo13 <rsantangelo13@gmail.com>
2020-12-01 21:28:36 -0500

    add api support for messages

--- backend/pkg/apiServer/apiServer.go
+++ backend/pkg/apiServer/apiServer.go
@@ -3,6 +3,7 @@ package apiServer
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"realtime-chat-go-react/pkg/database"
 	"strconv"
@@ -20,6 +21,19 @@ func returnAllUserChats(w http.ResponseWriter, r *http.Request) {
 
 func createNewUserChat(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var userIds []int64
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = json.Unmarshal(body, &userIds)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	database.CreateNewUserChat(userIds)
 }
 
 func deleteUserChat(w http.ResponseWriter, r *http.Request) {
@@ -27,7 +41,6 @@ func deleteUserChat(w http.ResponseWriter, r *http.Request) {
 }
 
 func getRecentMesages(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("asdfasdf")
 	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	i, _ := strconv.ParseInt(vars["id"], 10, 64)
@@ -37,10 +50,35 @@ func getRecentMesages(w http.ResponseWriter, r *http.Request) {
 
 func getRecentMesagesBefore(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	chatID, _ := strconv.ParseInt(vars["chatid"], 10, 64)
+	messageID, _ := strconv.ParseInt(vars["messageid"], 10, 64)
+	messages, _ := database.GetRecentMesagesBefore(chatID, messageID)
+	json.NewEncoder(w).Encode(messages)
 }
 
 func createNewMessage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var message string
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = json.Unmarshal(body, &message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	vars := mux.Vars(r)
+	chatID, _ := strconv.ParseInt(vars["chatid"], 10, 64)
+	userID, _ := strconv.ParseInt(vars["userid"], 10, 64)
+	fmt.Println(chatID)
+	fmt.Println(userID)
+	fmt.Println(body)
+	fmt.Println(message)
+	database.CreateNewMessage(chatID, userID, message)
 }
 
 func getChatPreferences(w http.ResponseWriter, r *http.Request) {
@@ -61,12 +99,12 @@ func setUserPreferences(w http.ResponseWriter, r *http.Request) {
 
 func SetRoutes(myRouter *mux.Router) {
 	myRouter.HandleFunc("/chats/user/{id}", returnAllUserChats).Methods("GET")
-	myRouter.HandleFunc("/chat/user/{id}", createNewUserChat).Methods("POST")
+	myRouter.HandleFunc("/chat", createNewUserChat).Methods("POST")
 	myRouter.HandleFunc("/chat/{id}/user/{id}", deleteUserChat).Methods("DELETE")
 
 	myRouter.HandleFunc("/chat/{id}/messages", getRecentMesages).Methods("GET")
-	myRouter.HandleFunc("/chat/{id}/messages/{id}", getRecentMesagesBefore).Methods("GET")
-	myRouter.HandleFunc("/user/{id}/chat/{id}/message", createNewMessage).Methods("POST")
+	myRouter.HandleFunc("/chat/{chatid}/messages/{messageid}", getRecentMesagesBefore).Methods("GET")
+	myRouter.HandleFunc("/chat/{chatid}/user/{userid}/message", createNewMessage).Methods("POST")
 
 	myRouter.HandleFunc("/chat/{id}/preferences/user/{id}", getChatPreferences).Methods("GET")
 	myRouter.HandleFunc("/chat/{id}/preferences/user/{id}", setChatPreferences).Methods("POST")
--- backend/pkg/database/databaseQuery.go
+++ backend/pkg/database/databaseQuery.go
@@ -50,8 +50,47 @@ func ReturnAllUserChats(userID int64) (map[int64]*model.Chat, error) {
 	return chats, err
 }
 
-func createNewUserChat(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+func CreateNewUserChat(users []int64) error {
+	var sqlQueryCreateChat = "INSERT into chat (messageTable) VALUES(0)"
+	var sqlQueryCreatePreferences = "INSERT into userchatpreferences (idChat, idUser) VALUES(?,?)"
+
+	tx, err := getMySQLConnection().Begin()
+	if err != nil {
+		return err
+	}
+
+	//intert new chat
+	stmt, err := tx.Prepare(sqlQueryCreateChat)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec()
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	//get new chat id
+	id, _ := res.LastInsertId()
+	fmt.Println(id)
+
+	//insert user chat preferences for all users in chat
+	stmt2, err := tx.Prepare(sqlQueryCreatePreferences)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	defer stmt2.Close()
+
+	for _, user := range users {
+		if _, err := stmt2.Exec(id, user); err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+	return tx.Commit()
 }
 
 func deleteUserChat(w http.ResponseWriter, r *http.Request) {
@@ -59,8 +98,7 @@ func deleteUserChat(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetRecentMesages(chatID int64) ([]model.Message, error) {
-	fmt.Println(chatID)
-	var sqlQuery = "SELECT idMessages0, idSentByUser, message, createdTime FROM messages0 where idChat = ? order by createdTime"
+	var sqlQuery = "SELECT idMessages0, idSentByUser, message, createdTime FROM messages0 where idChat = ? order by createdTime desc, idMessages0 desc limit 50"
 	stmt, err := getMySQLConnection().Prepare(sqlQuery)
 	defer closeStmt(stmt)
 	messages := []model.Message{}
@@ -73,16 +111,40 @@ func GetRecentMesages(chatID int64) ([]model.Message, error) {
 	if err != nil {
 		return messages, err
 	}
+	return getRecentMesages(res, messages)
+}
+
+func GetRecentMesagesBefore(chatID int64, messageId int64) ([]model.Message, error) {
+	fmt.Println(chatID)
+	fmt.Println(messageId)
+	var sqlQuery = "SELECT idMessages0, idSentByUser, message, createdTime FROM mydb.messages0 where idChat = ? AND createdTime <= (Select createdTime from mydb.messages0 where idMessages0 = ? limit 1) AND idMessages0 < ? order by createdTime desc, idMessages0 desc limit 50"
+	stmt, err := getMySQLConnection().Prepare(sqlQuery)
+	defer closeStmt(stmt)
+	messages := []model.Message{}
+
+	if err != nil {
+		return messages, err
+	}
+	res, err := stmt.Query(chatID, messageId, messageId)
+	defer closeRows(res)
+	if err != nil {
+		return messages, err
+	}
+
+	return getRecentMesages(res, messages)
+}
+
+func getRecentMesages(res *sql.Rows, messages []model.Message) ([]model.Message, error) {
 	for res.Next() {
 		var idMessages0 int64
 		var idSentByUser int64
 		var message string
 		var createdTime string
 
-		err = res.Scan(&idMessages0, &idSentByUser, &message, &createdTime)
+		err := res.Scan(&idMessages0, &idSentByUser, &message, &createdTime)
 		if err != nil {
 			return messages, err
-		}s
+		}
 		var newMessage = model.Message{
 			MessageId:   idMessages0,
 			Message:     message,
@@ -92,15 +154,34 @@ func GetRecentMesages(chatID int64) ([]model.Message, error) {
 		messages = append(messages, newMessage)
 
 	}
-	return messages, err
+	return messages, nil
 }
 
-func getRecentMesagesBefore(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-}
+func CreateNewMessage(chatId int64, userId int64, message string) (int64, error) {
+	var sqlQuery = "INSERT into messages0 (idChat, idSentByUser, message) VALUES(?,?,?)"
 
-func createNewMessage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	tx, err := getMySQLConnection().Begin()
+	if err != nil {
+		return -1, err
+	}
+
+	//intert new chat
+	stmt, err := tx.Prepare(sqlQuery)
+	if err != nil {
+		tx.Rollback()
+		return -1, err
+	}
+	defer stmt.Close()
+	res, err := stmt.Exec(chatId, userId, message)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return -1, err
+	}
+	//get new chat id
+	id, _ := res.LastInsertId()
+
+	return id, tx.Commit()
 }
 
 func getChatPreferences(w http.ResponseWriter, r *http.Request) {
